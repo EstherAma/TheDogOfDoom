dca9df2f1f42850715d3ab91e11065b4aa5d123d
Alba <albamrus@gmail.com>
2022-11-16 15:44:00 +0100

    js started

---
+++ js/index.js
@@ -0,0 +1,43 @@
+let coders = [
+  "Paula",
+  "Adriana",
+  "Veronika",
+  "Lola G",
+  "Natalia",
+  "Elena",
+  "Noa",
+  "Ana",
+  "Vero",
+  "Carmen",
+  "Paloma P",
+  "Raquel",
+  "Esther",
+  "Lola N",
+  "Alba",
+  "Sierri",
+  "Bea",
+  "Alesia",
+  "Carmen G",
+  "Camila",
+  "Leidy",
+  "Himo",
+  "Paloma B",
+  "Flor",
+  "Sandra",
+];
+
+let freeCoders = [];
+
+function free() {
+  let randomIndex = coders[Math.round(Math.random() * coders.length)];
+  for (let i = 0; i < coders.length; i++) {
+    if (coders[i] === randomIndex) {
+      coders.splice(i, 1);
+    }
+  }
+  /*  return randomIndex; */
+  console.log(randomIndex);
+}
+
+let boton = document.getElementById("btn");
+boton.addEventListener("click", free);
