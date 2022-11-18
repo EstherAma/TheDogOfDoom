4c61c4c72312535cd47e4a09d5dc6659101b0563
raquel2002x <palomofuentesraquel2002@gmail.com>
2022-11-18 10:58:10 +0100

    js first modification

--- js/index.js
+++ js/index.js
@@ -32,22 +32,20 @@ let button = document.getElementById("btn");
 button.addEventListener("click", free); // Escucha de eventos. Un click. Que queréis que pase cuando el ratón pasa por encima.
 showBallList(); // llamamos a la función para mostrar la lista por primera vez. Para que salga cuando entramos en la página
 
-function freeCoders() {  //cambiar coders
+function freeCoders() {  
     
-    let randomIndex = coders[Math.round(Math.random() * (coders.length - 1))]; // (coders.length - 1) si no pongo paréntesis se ejecuta primero la multiplicación
-    for (let index = 0; index < coders.length; index++) {    // round: redondea al más cercano. Ceil: redondea hacia arriba. Floor: redondea hacia abajo.
-        if (coders[index] === randomIndex) { 
+    let randomIndex = Math.round(Math.random() * (coders.length - 1)); // (coders.length - 1) si no pongo paréntesis se ejecuta primero la multiplicación
+    
             /* = atribuye el valor de la derecha al mismo de la izquierda. Un solo igual sería asignar
             === el mismo valor con el mismo tipo. El doble y el triple para comparara, condicionales
             == el mismo valor pero con distinto tipo*/ //por convencción siempre poner tres. Por seguridad.
-        coders.splice(index, 1); // splice: pasamos obligatoriamente dos parámetros separados por coma. Uno es el número del índice por donde queremos empezar, en nuestro es todo el index porque es aleatorio. El segundo es la cantidad de valores que queremos sacar.
+        coders.splice(randomIndex, 1); // splice: pasamos obligatoriamente dos parámetros separados por coma. Uno es el número del índice por donde queremos empezar, en nuestro es todo el index porque es aleatorio. El segundo es la cantidad de valores que queremos sacar.
         console.log(randomIndex); // ver en consola
-        }
+        
             addToKennel(coders);
             showBallList();
             showKennelList();
-        }
-    }
+}
 
 function showBallList() {
     let screen = "" // inicializar una cadena vacía
