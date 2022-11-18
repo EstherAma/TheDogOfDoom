176b1497e5569b142fa04ab03fdf8c340d565b3b
ZLoln <lolanavalo@gmail.com>
2022-11-18 10:33:15 +0100

    several coder animation trials

--- js/animation.js
+++ js/animation.js
@@ -4,18 +4,19 @@ let timer = setInterval(function() {
   // ¿Cuánto tiempo pasó desde el principio?
     let timePassed = Date.now() - start;
 
-    if (timePassed >= 2000) {
-    clearInterval(timer); // terminar la animación después de 2 segundos
+    if (timePassed >= 5200) {
+    clearInterval(timer); // terminar la animación después de 5,2 segundos
     return;
     }
 
   // dibujar la animación en el momento timePassed
-    draw(timePassed);
+    moveCoder(timePassed);
 
 }, 20);
 
-// mientras timePassed va de 0 a 2000
-// left obtiene valores de 0px a 400px
-function draw(timePassed) {
-    train.style.left = timePassed / 5 + 'px';
+// mientras timePassed va de 0 a 5200
+// left obtiene valores de 0px a 1040px
+function moveCoder(timePassed) {
+    coder.style.left = timePassed / 5 + 'px';
+    coder.style.bottom = timePassed / -5 + 'px';
 }
